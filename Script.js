0c1eccfe1b790fb9a8d3ec7d11af43532f62d803
ndddaapoouuu <vellyceblong@gmail.com>
2025-09-30 00:10:20 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,5 @@
-
+const tombolSapa=document.getElementById('sapaButton):
+tombolSapa.addEventListener('click',function() (
+  alert('halllo! Terimakasih suda berkunjung!');
+});
+                                         
